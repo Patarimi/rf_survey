c50b5bb850fb3a814defb1c7d5d57555557c5d58
Patarimi <38954040+Patarimi@users.noreply.github.com>
2024-03-14 17:09:50 +0100

    add plotting

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
+import matplotlib.pyplot as plt
 import streamlit as st
+import pandas as pd
 
 if __name__ == "__main__":
     st.set_page_config(
@@ -12,3 +14,9 @@ if __name__ == "__main__":
         },
     )
     st.title("Welcome")
+    data = pd.read_excel("data/PA-Survey-v8.xlsx", sheet_name="CMOS")
+    col1, col2 = st.columns(2)
+    col1.write(data.keys())
+    fig, ax = plt.subplots()
+    data.plot(x="Frequency (GHz)", y="Psat (dBm)", kind="scatter", ax=ax, logx=True)
+    col2.pyplot(fig)
