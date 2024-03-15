97ddd7280bd8b1d7ad18a496a8cf6fdbfcceff27
Patarimi <38954040+Patarimi@users.noreply.github.com>
2024-03-15 17:38:52 +0100

    add techno selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,27 +14,35 @@ if __name__ == "__main__":
             "Report a bug": "https://github.com/Patarimi/rf-survey/issues",
         },
     )
-    st.title("Welcome !")
+    st.title("Welcome!")
+    st.write("Please select a technology.")
+    techno = st.selectbox("Technology", ["CMOS", "SiGe", "GaN", "GaAs", "InP", "LDMOS", "Others"])
     cmap = mpl.colormaps.get_cmap("tab10").colors
-    data = pd.read_excel(io="data/PA-Survey-v8.xlsx", sheet_name="CMOS", usecols="B:V")
+    data = pd.read_excel(io="data/PA-Survey-v8.xlsx", sheet_name=techno, usecols="B:V")
     col1, col2 = st.columns([0.3, 0.7])
-    x_name = col1.selectbox('X axis', data.keys(), index=6)
-    y_name = col1.selectbox('Y axis', data.keys(), index=7)
-    x_log = col1.checkbox('X Log scale', True)
-    y_log = col1.checkbox('Y Log scale', False)
+    x_name = col1.selectbox("X axis", data.keys(), index=6)
+    y_name = col1.selectbox("Y axis", data.keys(), index=7)
+    x_log = col1.checkbox("X Log scale", True)
+    y_log = col1.checkbox("Y Log scale", False)
     fig, ax = plt.subplots()
-    for i, process in enumerate(data['Process'].unique()):
-        subset = data.loc[data['Process'] == process]
-        subset.plot(x=x_name,
-                    y=y_name, kind="scatter",
-                    ax=ax, logx=x_log, logy=y_log, label=process,
-                    color=cmap[i % 10])
+    for i, process in enumerate(data["Process"].unique()):
+        subset = data.loc[data["Process"] == process]
+        subset.plot(
+            x=x_name,
+            y=y_name,
+            kind="scatter",
+            ax=ax,
+            logx=x_log,
+            logy=y_log,
+            label=process,
+            color=cmap[i % 10],
+        )
     col2.pyplot(fig)
 
     st.write(
-        '**Source** : Hua Wang, Kyungsik Choi, Basem Abdelaziz, Mohamed Eleraky, Bryan Lin, Edward Liu, Yuqi Liu, '
-        'Hossein Jalili, Mohsen Ghorbanpoor, Chenhao Chu, Tzu-​Yuan Huang, Naga Sasikanth Mannem, Jeongsoo Park, '
-        'Jeongseok Lee, David Munzer,Sensen Li, Fei Wang, Amr S. Ahmed, Christopher Snyder, Huy Thong Nguyen, '
+        "**Source** : Hua Wang, Kyungsik Choi, Basem Abdelaziz, Mohamed Eleraky, Bryan Lin, Edward Liu, Yuqi Liu, "
+        "Hossein Jalili, Mohsen Ghorbanpoor, Chenhao Chu, Tzu-​Yuan Huang, Naga Sasikanth Mannem, Jeongsoo Park, "
+        "Jeongseok Lee, David Munzer,Sensen Li, Fei Wang, Amr S. Ahmed, Christopher Snyder, Huy Thong Nguyen, "
         'and Michael Edward Duffy Smith, "*Power Amplifiers Performance Survey 2000-​Present,*" '
-        '[Online]. Available: https://ideas.ethz.ch/Surveys/pa-​survey.html'
+        "[Online]. Available: https://ideas.ethz.ch/Surveys/pa-​survey.html"
     )
