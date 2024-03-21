7df5da534f3b4d59eebc2e0db56b55b09bb91de0
Patarimi <38954040+Patarimi@users.noreply.github.com>
2024-03-21 16:43:42 +0100

    further data cleaning

--- data_preparation.py
+++ data_preparation.py
@@ -20,7 +20,7 @@ class PASpec(BaseModel):
     pae_max: float = Field(..., ge=0, le=100)
     P1dB: float
     PAE_1dB: float
-    Gain: float
+    gain: float
     EVM: float
     modulation_speed: float
     average_pout: float
@@ -51,7 +51,7 @@ if __name__ == "__main__":
                 "PAEmax (%)": "pae_max",
                 "P1dB (dBm)": "P1dB",
                 "PAE_1dB (%)": "PAE_1dB",
-                "Gain (dB)": "Gain",
+                "Gain (dB)": "gain",
                 "EVM (dB)": "EVM",
                 "Modulation Speed (Msym/s)": "modulation_speed",
                 "Average Pout (dBm)": "average_pout",
@@ -62,7 +62,7 @@ if __name__ == "__main__":
             inplace=True,
         )
         clean_data = pd.DataFrame(columns=PASpec.model_fields)
-        for d in data.to_dict(orient="records"):
+        for i, d in enumerate(data.to_dict(orient="records")):
             match str(d["process"]).strip():
                 case "CMOS_Bulk" | "CMOS":
                     d["process"] = Process.bulk
@@ -87,16 +87,29 @@ if __name__ == "__main__":
                     d["PA_type"] = PAType.analog
                 case nan:
                     d["PA_type"] = PAType.unknown
-            if d["month"] is np.nan or d["month"] == "Early Access":
+            if d["month"] == "Early Access":
                 d["month"] = 0
-            if d["frequency"] is np.nan:
-                d["frequency"] = 0
-            d["node"] = str(d["node"]).lower().strip().replace("nm", "")
-            d["node"] = 0 if d["node"] == "na" else d["node"]
+            d["node"] = str(d["node"]).lower().rstrip().replace("nm", "")
+            d["modulation_speed"] = str(d["modulation_speed"]).lower().rstrip("-sy/mbpsc ")
+            for field in ("month", "year", "frequency", "pae_max", "average_pae", "average_pout", "gain", "sat_power", "P1dB", "PAE_1dB", "node"):
+                d[field] = str(d[field]).lower().rstrip("^(sde)* ")
+                if d[field] is np.nan or d[field] == "nan":
+                    d[field] = 0 if field != "year" else 1800
+                try:
+                    d[field] = float(d[field])
+                except ValueError:
+                    logging.info(f"line {i}: Error in {field} field: {d[field]}")
+                    d[field] = 0
+            if d["modulation_type"] is np.nan:
+                d["modulation_type"] = "unknown"
+
             try:
                 cd = PASpec(**d)
                 clean_data.loc[len(clean_data)] = dict(cd)
             except ValidationError as e:
-                logging.info(e)
+                if str(d["author_name"]) == "nan":
+                    # These are empty rows at the end of the sheet
+                    continue
+                logging.info(f"line {i}: {e}")
                 continue
         clean_data.to_csv(f"data/cleaned/{techno}.csv")
