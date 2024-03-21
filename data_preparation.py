6539d0a8626e1e0c4a56b25b8077c05939ce27fc
Patarimi <38954040+Patarimi@users.noreply.github.com>
2024-03-21 12:00:06 +0100

    renaming Process column

--- data_preparation.py
+++ data_preparation.py
@@ -2,5 +2,10 @@ import pandas as pd
 
 if __name__ == "__main__":
     for techno in ["CMOS", "SiGe", "GaN", "GaAs", "InP", "LDMOS", "Others"]:
-        data = pd.read_excel(io="data/raw/PA-Survey-v8.xlsx", sheet_name=techno, usecols="B:V")
+        data = pd.read_excel(
+            io="data/raw/PA-Survey-v8.xlsx", sheet_name=techno, usecols="B:V"
+        )
+        data.rename(
+            columns={"Process (CMOS_Bulk, CMOS_SOI, SiGe)": "Process"}, inplace=True
+        )
         data.to_csv(f"data/cleaned/{techno}.csv")
