5ed422c2b5309ccec416405328f6071398043a55
Patarimi <38954040+Patarimi@users.noreply.github.com>
2024-06-24 15:03:16 +0200

    change backend to plotly add data selection and coloring

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,8 @@
 import streamlit as st
 import pandas as pd
-from bokeh.plotting import figure
-from bokeh.palettes import Spectral6
-from bokeh.models import HoverTool
 import numpy as np
-from scipy.spatial import ConvexHull
 from menu import menu
+import plotly.express as px
 
 
 @st.cache_data
@@ -44,10 +41,12 @@ if __name__ == "__main__":
             "Technology", ["CMOS", "SiGe", "GaN", "GaAs", "InP", "LDMOS", "Others"]
         )
         data = load_data(f"data/cleaned/{techno}.csv")
-        sel_tech = dict()
+        sel_tech = list()
         popover = st.popover("Technology Filter")
         for sub_t in data["process"].unique():
-            sel_tech[sub_t] = popover.checkbox(sub_t.split(".")[-1], value=True)
+            val = popover.checkbox(sub_t.split(".")[-1], value=True)
+            if val:
+                sel_tech.append(sub_t)
         col1.write("---")
         c1, c2 = st.columns([0.5, 0.5])
         data["ITRS FOM"] = (
@@ -57,67 +56,37 @@ if __name__ == "__main__":
         x_name = c1.selectbox("X axis", field_list, index=1)
         y_name = c2.selectbox("Y axis", field_list, index=2)
         x_log = c1.checkbox("X Log scale", True)
-        x_min, x_max = np.min(data[x_name]), np.max(data[x_name])
-        x_min_u, x_max_u = col1.slider("Rescale X", x_min, x_max, (x_min, x_max))
         y_log = c2.checkbox("Y Log scale", False)
-        draw_hull = col1.checkbox("SoA contour", True)
+        colored = c1.checkbox("Colored", True)
+        if colored:
+            color_name = c1.selectbox(
+                "Color", np.append(field_list.values, "process"), index=3
+            )
+        marker = c2.checkbox("Marker", False)
+        if marker:
+            marker_name = c2.selectbox("Marker", field_list, index=12)
     if col1.button("clear data"):
         load_data.clear()
-    tooltips = "@author_name:@year"
-    p = figure(
-        title=f"State of Art of PA in {techno}",
-        x_axis_label=x_name,
-        y_axis_label=y_name,
-        x_axis_type="log" if x_log else "linear",
-        y_axis_type="log" if y_log else "linear",
+    fig = px.scatter(
+        data[data["process"].isin(sel_tech)],
+        x=x_name,
+        y=y_name,
+        color=None if not colored else color_name,
+        symbol=None if not (marker) else marker_name,
+        hover_name="author_name",
+        hover_data=["year"],
+        log_x=x_log,
+        log_y=y_log,
+        labels={x_name: x_name, y_name: y_name},
     )
-    hvr = HoverTool(tooltips=tooltips)
-    p.add_tools(hvr)
-    sca = list()
-    for i, process in enumerate(sel_tech):
-        if not sel_tech[process]:
-            continue
-        subset = data.loc[
-            (data["process"] == process)
-            & (data[x_name] >= x_min_u)
-            & (data[x_name] <= x_max_u),
-            [x_name, y_name, "year", "author_name"],
-        ].dropna()
-        sca.append(
-            p.scatter(
-                x=x_name,
-                y=y_name,
-                source=subset,
-                marker="o",
-                color=Spectral6[i % 6],
-                legend_label=process.split(".")[-1],
-            )
+    event = col2.plotly_chart(fig, use_container_width=True, on_select="rerun")
+    if event:
+        st.write("## Selected Data:")
+        st.write(
+            data.drop(columns=["Unnamed: 0", "month"]).loc[
+                data.index.isin(event.selection.point_indices)
+            ]
         )
-        hull_set = subset.loc[(data["process"] == process), [x_name, y_name]]
-        if x_log:
-            hull_set[x_name] = np.log10(hull_set[x_name])
-        if y_log:
-            hull_set[y_name] = np.log10(hull_set[y_name])
-        if not draw_hull:
-            continue
-        if hull_set.shape[0] > 3:
-            array = np.array(hull_set.dropna())
-            hull = ConvexHull(array)
-            x, y = list(), list()
-            for simplex in hull.vertices:
-                x.append(10 ** array[simplex, 0] if x_log else array[simplex, 0])
-                y.append(10 ** array[simplex, 1] if y_log else array[simplex, 1])
-            p.patch(x, y, color=Spectral6[i % 6], fill_alpha=0.1, line_alpha=1)
-        else:
-            p.patch(
-                hull_set[x_name],
-                hull_set[y_name],
-                color=Spectral6[i % 6],
-                fill_alpha=0.1,
-                line_alpha=1,
-            )
-    hvr.renderers = sca
-    col2.bokeh_chart(p, use_container_width=True)
     col2.write(
         "**Source** : Hua Wang, Kyungsik Choi, Basem Abdelaziz, Mohamed Eleraky, Bryan Lin, Edward Liu, Yuqi Liu, "
         "Hossein Jalili, Mohsen Ghorbanpoor, Chenhao Chu, Tzu-Yuan Huang, Naga Sasikanth Mannem, Jeongsoo Park, "
