bf0048962e04824a527eabeb652ebcc99738d3d6
Patarimi <38954040+Patarimi@users.noreply.github.com>
2024-08-20 14:32:23 +0200

    improve data edition

--- pages/edit_data.py
+++ pages/edit_data.py
@@ -1,13 +1,16 @@
+import glob
+from os.path import basename
 import streamlit as st
 from pages.menu import menu_with_redirect
 import pandas as pd
+from processing.data_preparation import PASpec
 
 # Redirect to app.py if not logged in, otherwise show the navigation menu
 menu_with_redirect()
 
 st.title("Data Edition Page")
-st.markdown(f"You are currently logged with the role of {st.session_state.role}.")
-
-path = "data/cleaned/CMOS.csv"
-df = pd.read_csv(path)
-st.data_editor(df, key="data_editor")
+tu = glob.glob("data/cleaned/*")
+selected = st.selectbox("Please select files", [basename(file) for file in tu])
+path = f"data/cleaned/{selected}"
+df = pd.read_csv(path, usecols=PASpec.model_fields)
+corrected = st.data_editor(df, key="data_editor", hide_index=True)
